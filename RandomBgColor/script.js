4579d5445d66a1a97e518fbdec22ba7f9360f0f0
Sunny <sobhit230@gmail.com>
2024-02-24 15:52:15 +0530

    Background Color Changer Completed

--- RandomBgColor/script.js
+++ RandomBgColor/script.js
@@ -1,1 +1,25 @@
+const btn = document.querySelector('button');
+const h1 = document.querySelector('h1');
+const body = document.querySelector('body');
+
+function randColor() {
+    let str = "0123456789abcdef";
+
+    let res = "#";
+    for (let i = 0; i < 6; i++) {
+        let num = Math.floor(Math.random() * 16);
+        res += str.charAt(num);
+    }
+
+    update(res);
+
+    
+}
+
+function update(res){
+    body.style.backgroundColor = `${res}`
+    h1.innerHTML = `BgColor:${res}`
+}
+
+btn.addEventListener('click',randColor);
 
